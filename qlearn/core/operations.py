3f6d2534c865b3e3a32a26e5fb9f43016a246058
Tingting-Chuzhi-Xu <chuzhi.x@wustl.edu>
2024-06-24 15:36:38 -0400

    initial_test

---
+++ qlearn/core/operations.py
@@ -0,0 +1,18 @@
+import numpy
+import scipy.sparse
+import scipy.sparse.linalg
+import tensorflow as tf
+import qutip as qt
+import pdb
+
+def test_func():
+    return 1
+
+class OpMatrices:
+
+    def identity(self, N):
+        return 1
+        #return tf.eye(N)
+    
+
+
---
+++ qlearn/test/test.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('../')
+
+#from core.operations import OpMatrices
+from core.operations import test_func
+
+
+print("hello")
